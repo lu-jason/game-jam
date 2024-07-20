b3a21c517c39595e7be94604f8bae09415b2c0da
J Kouts <joshkoutsouridis@gmail.com>
2024-07-20 17:36:34 +0930

    working debounce of morphing actions

--- GameJam/Main.cs
+++ GameJam/Main.cs
@@ -3,31 +3,38 @@ using System;
 
 public partial class Main : Node2D
 {
+	public double morphTimer = 0.0;
+	public bool canMorph = true;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		// Morphing state
-		// TODO - may need throttling/debounce to ensure not super spam of morphing
-		if (Input.IsActionPressed("morph_witch")) {
-			GD.Print("morphing into witch"); // 1 + num1
-		}
-		if (Input.IsActionPressed("morph_fox")) {
-			GD.Print("morphing into fox"); // 2 + num2
-		}
-		if (Input.IsActionPressed("morph_salamander")) {
-			GD.Print("morphing into salamander"); // 3 + num3
-		}
-		if (Input.IsActionPressed("morph_gargoyle")) {
-			GD.Print("morphing into gargoyle"); // 4 + num4
-		}
+		if (canMorph) { // ! Mainly here for conditions where player is not allowed to morph (idk what that is yet) - Josh
+			morphTimer += delta;
 
-
+			if (Input.IsActionPressed("morph_witch") && morphTimer > 0.5) {
+				GD.Print("morphing into witch"); // 1 + num1
+				morphTimer = 0;
+			}
+			if (Input.IsActionPressed("morph_fox") && morphTimer > 0.5) {
+				GD.Print("morphing into fox"); // 2 + num2
+				morphTimer = 0;
+			}
+			if (Input.IsActionPressed("morph_salamander") && morphTimer > 0.5) {
+				GD.Print("morphing into salamander"); // 3 + num3
+				morphTimer = 0;
+			}
+			if (Input.IsActionPressed("morph_gargoyle") && morphTimer > 0.5) {
+				GD.Print("morphing into gargoyle"); // 4 + num4
+				morphTimer = 0;
+			}
+		}
 	}
 
 	public override void _Draw() {
