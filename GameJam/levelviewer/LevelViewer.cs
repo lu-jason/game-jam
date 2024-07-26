9c2c7643a15e00287fd6c769747d637110f2b1ca
Jason Lu <jason_lu_97@hotmail.com>
2024-07-26 18:01:29 +0930

    separate out some functionality

--- GameJam/levelviewer/LevelViewer.cs
+++ GameJam/levelviewer/LevelViewer.cs
@@ -3,7 +3,8 @@ using Godot.Collections;
 using System;
 using System.Numerics;
 
-public partial class LevelViewer : Node2D {
+public partial class LevelViewer : Node2D
+{
 	[Export]
 	public PackedScene Map { get; set; }
 
@@ -18,31 +19,25 @@ public partial class LevelViewer : Node2D {
 
 	private TileMap Level;
 
-	private Vector2I TileBounds;
+	private Vector2I tileBounds;
 
-	private GameObject[,] GameObjects;
+	private GameObject[,] gameObjects;
 
 	private Dictionary<string, int> LayerMap = new Dictionary<string, int>();
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() {
+	public override void _Ready()
+	{
 		Level = Map.Instantiate<TileMap>();
 		Level.ZIndex = -1;
 
 		AddChild(Level);
 		EmitSignal(SignalName.OnLoadLevel, Level);
 
-		for (int i = 0; i < Level.GetLayersCount(); i++) {
-			LayerMap.Add(Level.GetLayerName(i), i);
-		}
+		SetupLevelInfo();
 
 		// Later change this to be called whenever we move a light or something can change it maybe
 		EmitSignal(SignalName.OnLightsChanged, Level);
-
-		// Initiate lightLevels to match loadedLevel size
-		TileBounds = Level.GetUsedRect().End;
-
-		GameObjects = new GameObject[TileBounds.X, TileBounds.Y];
 
 		// TODO Find spawn point
 		// For now hardcoded
@@ -52,66 +47,98 @@ public partial class LevelViewer : Node2D {
 		CreateObjects();
 	}
 
-	private void CreateObjects() {
+	private void SetupLevelInfo()
+	{
+		for (int i = 0; i < Level.GetLayersCount(); i++)
+		{
+			LayerMap.Add(Level.GetLayerName(i), i);
+		}
 
-		Vector2I tileBounds = Level.GetUsedRect().End;
+		// Initiate lightLevels to match loadedLevel size
+		tileBounds = Level.GetUsedRect().End;
 
+		gameObjects = new GameObject[tileBounds.X, tileBounds.Y];
+	}
 
+	private void CreateObjects()
+	{
 		// Iterate through all the objects in the "objects" layer in the tilemap
 		// and create the corresponding object and delete the cell from the map
-		for (int x = 0; x < tileBounds.X; x++) {
-			for (int y = 0; y < tileBounds.Y; y++) {
+		for (int x = 0; x < tileBounds.X; x++)
+		{
+			for (int y = 0; y < tileBounds.Y; y++)
+			{
 				var tileData = GetTileData("objects", new Vector2I(x, y));
 
-				if (tileData != null && tileData.GetCustomData("Rock").AsBool()) {
+				if (tileData != null && tileData.GetCustomData("Rock").AsBool())
+				{
 					var position = new Vector2I(x, y);
 					Level.SetCell(GetLayerNumber("objects"), position, -1);
 					GD.Print("Found rock at ", x, y);
 					var rock = RockScene.Instantiate<Rock>();
 					AddChild(rock);
 					rock.MoveTo(position, "");
-					GameObjects[x, y] = rock;
+					AddGameObject(rock, position);
 				}
 			}
 		}
 	}
 
-	public bool IsObject(Vector2I position) {
-		if (GameObjects[position.X, position.Y] != null) {
+	private bool AddGameObject(GameObject obj, Vector2I position)
+	{
+		if (gameObjects[position.X, position.Y] == null)
+		{
+			gameObjects[position.X, position.Y] = obj;
 			return true;
 		}
 		return false;
 	}
 
-	public bool MoveObject(Vector2I position, string direction) {
-		if (IsObject(position)) {
-			var gameObject = GameObjects[position.X, position.Y];
-			switch (direction) {
+	public bool IsObject(Vector2I position)
+	{
+		if (gameObjects[position.X, position.Y] != null)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public bool MoveObject(Vector2I position, string direction)
+	{
+		if (IsObject(position))
+		{
+			var gameObject = gameObjects[position.X, position.Y];
+			switch (direction)
+			{
 				case "left":
-					if (gameObject.MoveLeft()) {
-						GameObjects[position.X - 1, position.Y] = gameObject;
-						GameObjects[position.X, position.Y] = null;
+					if (gameObject.MoveLeft())
+					{
+						gameObjects[position.X - 1, position.Y] = gameObject;
+						gameObjects[position.X, position.Y] = null;
 						return true;
 					}
 					break;
 				case "right":
-					if (gameObject.MoveRight()) {
-						GameObjects[position.X + 1, position.Y] = gameObject;
-						GameObjects[position.X, position.Y] = null;
+					if (gameObject.MoveRight())
+					{
+						gameObjects[position.X + 1, position.Y] = gameObject;
+						gameObjects[position.X, position.Y] = null;
 						return true;
 					}
 					break;
 				case "up":
-					if (gameObject.MoveUp()) {
-						GameObjects[position.X, position.Y - 1] = gameObject;
-						GameObjects[position.X, position.Y] = null;
+					if (gameObject.MoveUp())
+					{
+						gameObjects[position.X, position.Y - 1] = gameObject;
+						gameObjects[position.X, position.Y] = null;
 						return true;
 					}
 					break;
 				case "down":
-					if (gameObject.MoveDown()) {
-						GameObjects[position.X, position.Y + 1] = gameObject;
-						GameObjects[position.X, position.Y] = null;
+					if (gameObject.MoveDown())
+					{
+						gameObjects[position.X, position.Y + 1] = gameObject;
+						gameObjects[position.X, position.Y] = null;
 						return true;
 					}
 					break;
@@ -121,35 +148,44 @@ public partial class LevelViewer : Node2D {
 		return false;
 	}
 
-	private int GetLayerNumber(string layerName) {
+	private int GetLayerNumber(string layerName)
+	{
 		return LayerMap[layerName];
 	}
-	public override void _Input(InputEvent @event) {
+	public override void _Input(InputEvent @event)
+	{
 
 
 		// We can move this later if we want just adding for testing sake`
-		if (@event.IsActionPressed("place_light")) {
+		if (@event.IsActionPressed("place_light"))
+		{
 			CreateLightSource();
 		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+	public override void _Process(double delta)
+	{
 	}
 
-	public TileData GetTileData(string layerName, Vector2I coords) {
+	public TileData GetTileData(string layerName, Vector2I coords)
+	{
 		int layer = GetLayerNumber(layerName);
 		return Level.GetCellTileData(layer, coords);
 	}
 
-	public void OnToggleLightingPressed() {
+	public void OnToggleLightingPressed()
+	{
 		LightingManager lightingManager = GetNode<LightingManager>("LightingManager");
 		// Weirdly lightManager.Visible didn't like it when I tried to use it in a ternary operator.
 		bool isVisible = lightingManager.Visible;
-		if (isVisible) {
+		if (isVisible)
+		{
 			lightingManager.Hide();
 			lightingManager.ClearTiles();
-		} else {
+		}
+		else
+		{
 			lightingManager.Show();
 			// Repopulate tiles
 			EmitSignal(SignalName.OnLightsChanged, Level);
@@ -157,7 +193,8 @@ public partial class LevelViewer : Node2D {
 	}
 
 
-	private void CreateLightSource() {
+	private void CreateLightSource()
+	{
 		GD.Print("WARNING: CreateLightSource has been deactivated while I rework a bunch of shit");
 		// TODO place in front of the user instead of on the player.
 		// perhaps storing the direction the player is facing could be good
