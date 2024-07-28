e7c328e14c7cf51b30a8f6b4a279f4827753f7a0
Jason Lu <jason_lu_97@hotmail.com>
2024-07-28 18:34:28 +0930

    formatting

--- GameJam/levelviewer/LevelViewer.cs
+++ GameJam/levelviewer/LevelViewer.cs
@@ -3,8 +3,7 @@ using Godot.Collections;
 using System;
 using System.Numerics;
 
-public partial class LevelViewer : Node2D
-{
+public partial class LevelViewer : Node2D {
 	[Export]
 	public PackedScene Map { get; set; }
 
@@ -26,8 +25,7 @@ public partial class LevelViewer : Node2D
 	private Dictionary<string, int> LayerMap = new Dictionary<string, int>();
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
 		Level = Map.Instantiate<TileMap>();
 		Level.ZIndex = -1;
 
@@ -47,10 +45,8 @@ public partial class LevelViewer : Node2D
 		CreateObjects();
 	}
 
-	private void SetupLevelInfo()
-	{
-		for (int i = 0; i < Level.GetLayersCount(); i++)
-		{
+	private void SetupLevelInfo() {
+		for (int i = 0; i < Level.GetLayersCount(); i++) {
 			LayerMap.Add(Level.GetLayerName(i), i);
 		}
 
@@ -60,18 +56,14 @@ public partial class LevelViewer : Node2D
 		gameObjects = new GameObject[tileBounds.X, tileBounds.Y];
 	}
 
-	private void CreateObjects()
-	{
+	private void CreateObjects() {
 		// Iterate through all the objects in the "objects" layer in the tilemap
 		// and create the corresponding object and delete the cell from the map
-		for (int x = 0; x < tileBounds.X; x++)
-		{
-			for (int y = 0; y < tileBounds.Y; y++)
-			{
+		for (int x = 0; x < tileBounds.X; x++) {
+			for (int y = 0; y < tileBounds.Y; y++) {
 				var tileData = GetTileData("objects", new Vector2I(x, y));
 
-				if (tileData != null && tileData.GetCustomData("Rock").AsBool())
-				{
+				if (tileData != null && tileData.GetCustomData("Rock").AsBool()) {
 					var position = new Vector2I(x, y);
 					var objectStrength = GetObjectTileData(position).GetCustomData("Strength").AsInt32();
 					Level.SetCell(GetLayerNumber("objects"), position, -1);
@@ -87,65 +79,53 @@ public partial class LevelViewer : Node2D
 		}
 	}
 
-	private bool AddGameObject(GameObject obj, Vector2I position)
-	{
-		if (gameObjects[position.X, position.Y] == null)
-		{
+	private bool AddGameObject(GameObject obj, Vector2I position) {
+		if (gameObjects[position.X, position.Y] == null) {
 			gameObjects[position.X, position.Y] = obj;
 			return true;
 		}
 		return false;
 	}
 
-	public bool IsObject(Vector2I position)
-	{
+	public bool IsObject(Vector2I position) {
 		return gameObjects[position.X, position.Y] != null;
 	}
 
-	public GameObject GetObject(Vector2I position)
-	{
+	public GameObject GetObject(Vector2I position) {
 		return gameObjects[position.X, position.Y];
 	}
 
-	public TileData GetObjectTileData(Vector2I position)
-	{
+	public TileData GetObjectTileData(Vector2I position) {
 		return GetTileData("objects", position);
 	}
 
-	public bool MoveObject(Vector2I position, string direction)
-	{
-		if (IsObject(position))
-		{
+	public bool MoveObject(Vector2I position, string direction) {
+		if (IsObject(position)) {
 			var gameObject = gameObjects[position.X, position.Y];
-			switch (direction)
-			{
+			switch (direction) {
 				case "left":
-					if (gameObject.MoveLeft())
-					{
+					if (gameObject.MoveLeft()) {
 						gameObjects[position.X - 1, position.Y] = gameObject;
 						gameObjects[position.X, position.Y] = null;
 						return true;
 					}
 					break;
 				case "right":
-					if (gameObject.MoveRight())
-					{
+					if (gameObject.MoveRight()) {
 						gameObjects[position.X + 1, position.Y] = gameObject;
 						gameObjects[position.X, position.Y] = null;
 						return true;
 					}
 					break;
 				case "up":
-					if (gameObject.MoveUp())
-					{
+					if (gameObject.MoveUp()) {
 						gameObjects[position.X, position.Y - 1] = gameObject;
 						gameObjects[position.X, position.Y] = null;
 						return true;
 					}
 					break;
 				case "down":
-					if (gameObject.MoveDown())
-					{
+					if (gameObject.MoveDown()) {
 						gameObjects[position.X, position.Y + 1] = gameObject;
 						gameObjects[position.X, position.Y] = null;
 						return true;
@@ -157,44 +137,35 @@ public partial class LevelViewer : Node2D
 		return false;
 	}
 
-	private int GetLayerNumber(string layerName)
-	{
+	private int GetLayerNumber(string layerName) {
 		return LayerMap[layerName];
 	}
-	public override void _Input(InputEvent @event)
-	{
+	public override void _Input(InputEvent @event) {
 
 
 		// We can move this later if we want just adding for testing sake`
-		if (@event.IsActionPressed("place_light"))
-		{
+		if (@event.IsActionPressed("place_light")) {
 			CreateLightSource();
 		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
 	}
 
-	public TileData GetTileData(string layerName, Vector2I coords)
-	{
+	public TileData GetTileData(string layerName, Vector2I coords) {
 		int layer = GetLayerNumber(layerName);
 		return Level.GetCellTileData(layer, coords);
 	}
 
-	public void OnToggleLightingPressed()
-	{
+	public void OnToggleLightingPressed() {
 		LightingManager lightingManager = GetNode<LightingManager>("LightingManager");
 		// Weirdly lightManager.Visible didn't like it when I tried to use it in a ternary operator.
 		bool isVisible = lightingManager.Visible;
-		if (isVisible)
-		{
+		if (isVisible) {
 			lightingManager.Hide();
 			lightingManager.ClearTiles();
-		}
-		else
-		{
+		} else {
 			lightingManager.Show();
 			// Repopulate tiles
 			EmitSignal(SignalName.OnLightsChanged, Level);
@@ -202,8 +173,7 @@ public partial class LevelViewer : Node2D
 	}
 
 
-	private void CreateLightSource()
-	{
+	private void CreateLightSource() {
 		GD.Print("WARNING: CreateLightSource has been deactivated while I rework a bunch of shit");
 		// TODO place in front of the user instead of on the player.
 		// perhaps storing the direction the player is facing could be good
