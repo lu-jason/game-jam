4e2856a4f895aef283449fb8d5f47d4b3b7156b5
Mark Hubczenko <mark.hubczenko@gmail.com>
2024-07-30 20:34:36 +0930

    Resolved broken merge

--- GameJam/objects/Player.cs
+++ GameJam/objects/Player.cs
@@ -3,7 +3,6 @@ using Godot;
 
 public partial class Player : GameObject
 {
-
     public override bool CanMove(Vector2I coords, string direction)
     {
         // Later we could hold a reference to these so we don't need to look them up with this "hardcodey" way
@@ -44,7 +43,7 @@ public partial class Player : GameObject
         }
 
         // If the walls and shadows are empty we can move there
-        if ((wallData == null) && (shadowData == null))
+        if ((shadowData == null))
         {
             bool IsEnd = floorData.GetCustomData("RoomEnd").AsBool();
             if (IsEnd)
@@ -59,11 +58,31 @@ public partial class Player : GameObject
                 return false;
             }
 
+            // Yikes code written quickly
+            if (wallData != null)
+            {
+                // If the character can move 
+                if (WallMoveSpecific(wallData))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
             return true;
             // TODO - readd hole logic
         }
 
         // Do whatever logic here for now return true;
+        return false;
+    }
+
+    virtual public bool WallMoveSpecific(TileData wallData)
+    {
+        // Never allow walls in general player cases
         return false;
     }
     public override void SetAnimationState(string action, string direction)
